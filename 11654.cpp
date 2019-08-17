f04b1c71494cc9c081dad3ffbc108cd3f304adfd
gaon_choi <choigaon1028@gmail.com>
2019-08-17 21:22:52 +0900

    august_17th

---
+++ 11654.cpp
@@ -0,0 +1,10 @@
+//https://www.acmicpc.net/problem/11654
+
+#include <iostream>
+
+int main(void) {
+	char ch;
+	std::cin >> ch;
+	std::cout << static_cast<int>(ch);
+	return 0;
+}
---
+++ 15680.cpp
@@ -0,0 +1,15 @@
+//https://www.acmicpc.net/problem/15680
+
+#include <iostream>
+
+int main(void) {
+	int N;
+	std::cin >> N;
+	if (N == 1) {
+		std::cout << "Leading the Way to the Future";
+	}
+	else {
+		std::cout << "YONSEI";
+	}
+	return 0;
+}
