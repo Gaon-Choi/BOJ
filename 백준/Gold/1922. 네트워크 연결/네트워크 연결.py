74338517b872329a58223fcd5afe44ba5c97ee6e
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-03-18 18:59:38 +0900

    [Gold IV] Title: 네트워크 연결, Time: 272 ms, Memory: 55240 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+
+sys.setrecursionlimit(1000000)
+
+def find(n):
+    if uf[n] == n:
+        return n
+        
+    root_node = find(uf[n])
+    
+    uf[n] = root_node
+    
+    return root_node
+    
+def union(a, b):
+    A = find(a)
+    B = find(b)
+    
+    uf[A] = B
+
+V, E = 1000, 100000
+
+v = int(input())
+e = int(input())
+
+uf = list(range(V + 1))
+
+edges = []
+
+for _ in range(e):
+    # (A, B, C) : A와 B를 잇는 간선 (가중치 = C)
+    edges.append(list(map(int, input().split())))
+    
+edges.sort(key = lambda x:x[2])
+
+mst = 0
+
+for e in edges:
+    A, B, w = e[0], e[1], e[2]
+    
+    if find(A) != find(B):
+        union(A, B)
+        mst += w
+    
+print(mst)
