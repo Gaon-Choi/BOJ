f6b575dfb690996d67f3c22732a31c700eb8ee5a
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-03-18 19:07:55 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void) {
+	std::cout << "     /~\\" << std::endl;
+	std::cout << "    ( oo|" << std::endl;
+	std::cout << "    _\\=/_" << std::endl;
+	std::cout << "   /  _  \\" << std::endl;
+	std::cout << "  //|/.\\|\\\\" << std::endl;
+	std::cout << " ||  \\ /  ||" << std::endl;
+	std::cout << "============" << std::endl;
+	std::cout << "|          |" << std::endl;
+	std::cout << "|          |" << std::endl;
+	std::cout << "|          |";
+	return 0;
+}
