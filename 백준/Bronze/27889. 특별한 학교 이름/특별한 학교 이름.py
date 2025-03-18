0bcfd9b8acfa8bf07711a222b75155f700dc2324
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-03-18 19:19:00 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+univ = input()
+
+if univ == "NLCS":
+	print("North London Collegiate School")
+elif univ == "BHA":
+	print("Branksome Hall Asia")
+elif univ == "KIS":
+	print("Korea International School")
+elif univ == "SJA":
+	print("St. Johnsbury Academy")
