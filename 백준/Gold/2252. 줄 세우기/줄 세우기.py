3c6d5259f6dfb07a4450cbff9fcd16e8a006f54c
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-03-22 00:18:07 +0900

    [Gold III] Title: 줄 세우기, Time: 2640 ms, Memory: 39076 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,39 @@
+def topological_sort():
+    queue = []
+
+    res = []
+
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            queue.append(i)
+
+    while len(queue) > 0:
+        x = queue.pop(0)
+
+        res.append(x)
+
+        for v in graph[x]:
+            indegree[v] -= 1
+
+            if indegree[v] == 0:
+                queue.append(v)
+
+    return res
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+indegree = [0] * (n + 1)
+
+for _ in range(m):
+    # a > b <=> a -> b
+    a, b = map(int, input().split())
+
+    graph[a].append(b)
+
+    indegree[b] += 1
+
+result = topological_sort()
+
+for elem in result:
+    print(elem, end = " ")
