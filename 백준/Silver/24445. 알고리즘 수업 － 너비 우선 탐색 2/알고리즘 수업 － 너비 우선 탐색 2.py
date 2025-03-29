2d192fa7f9ceb880007e75532f5d8a38e313c624
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-03-30 05:06:52 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 844 ms, Memory: 132932 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def bfs(v):
+    cnt = 0
+
+    cnt += 1
+    visited[v] = cnt
+
+    queue = [v]
+
+    while queue:
+        x = queue.pop(0)
+
+        for adj in sorted(graph[x], reverse = True):
+            if not visited[adj]:
+                cnt += 1
+                visited[adj] = cnt
+                queue.append(adj)
+    
+
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+bfs(r)
+
+print(*visited[1:], sep="\n")
