5c63f53743db447436c1b0c322d13ad850db2ccd
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-04-28 09:42:23 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+dp = [1] * n
+
+for i in range(n):
+    temp = 0
+    for j in range(i):
+        if arr[i] > arr[j]:
+            temp = max(temp, dp[j])
+
+    dp[i] = temp + 1
+
+print(max(dp))
