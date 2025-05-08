e7f0e3ac84b44bff66c47bd26bd92b1d6540d8b0
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-05-09 00:17:45 +0900

    [Silver V] Title: 올림픽, Time: 100 ms, Memory: 109544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -10,16 +10,14 @@ for _ in range(n):
 
 arr.sort(key = lambda x : (-x[1], -x[2], -x[3]))
 
-rank = 1
-prev = arr[0]
+rank = 0
+prev = [0, 0, 0, 0]
 
 for i in range(n):
-    if prev == arr[i]:
-        continue
-    else:
+    if prev != arr[i][1:]:
         rank = i
     
-    prev = arr[i]
+    prev = arr[i][1:]
 
     if k == arr[i][0]:
-        print(rank)
+        print(rank + 1)
