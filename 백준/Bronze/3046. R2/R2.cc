bc5355b844bfcd8c2bf5912593786cac9edd73c8
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2026-01-03 23:11:02 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3046. R2/R2.cc
+++ 백준/Bronze/3046. R2/R2.cc
@@ -1,9 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 3046                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: choigaon1028_cpp <boj.kr/u/choigaon1028_c   +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/3046                           #+#        #+#      #+#    */
+/*   Solved: 2026/01/03 23:09:08 by choigaon1028_c###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
 #include <iostream>
 
-int main(void) {
-	int r1, r2, s;
-	std::cin >> r1 >> s;
-	r2 = 2 * s - r1;
-	std::cout << r2 << std::endl;
-	return 0;
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int R1, S;
+    cin >> R1 >> S;
+    cout << 2 * S - R1 << "\n";
+    return 0;
 }
